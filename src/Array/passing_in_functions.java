cb8bf23cda09808968c9c9309fc47c62208e67cd
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2022-09-11 16:34:36 +0530

    function in array

---
+++ src/Array/passing_in_functions.java
@@ -0,0 +1,16 @@
+package Array;
+
+import java.util.Arrays;
+
+public class passing_in_functions {
+    public static void main(String[] args) {
+        int[] num = {0,1,2,3,4,5,6,7};
+        System.out.println(Arrays.toString(num));
+        change(num);
+        System.out.println(Arrays.toString(num));
+    }
+
+    static void change(int[] arr){
+        arr[0] = 99;
+    }
+}
